b1294a8f909236fd5c2992b23b20a87a645f340b
Curtis Patterson <Dpatter94@yahoo.com>
2024-06-18 13:52:05 -0400

    Added Account to Nav, in account added logout, adn h1, more updates to come on that

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ProductList from "./pages/index";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Register from "./pages/register";
 import Login from "./pages/login";
+import Account from "./pages/account";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path={"/"} element={<ProductList/>}/>
-        <Route path={"/register"} element={<Register/>}/>
-        <Route path={"/login"} element={<Login/>}/>
+        <Route path={"/"} element={<ProductList />} />
+        <Route path={"/register"} element={<Register />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/account"} element={<Account />} />
       </Routes>
     </div>
   );
