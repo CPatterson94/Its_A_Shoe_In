86a71415afcdcfbe90d0622caa1558290d74003e
ojgraham1 <ojgraham1@gmail.com>
2024-06-18 13:12:01 -0400

    started nav and footer  can change later

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -4,12 +4,16 @@ import App from "./App";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import store from "./store";
+import Nav from './components/navigation/nav'
+import Footer from './components/navigation/footer'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
+                <Nav/>
                 <App/>
+                <Footer/>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>
