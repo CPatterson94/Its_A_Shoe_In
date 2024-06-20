96028722c800470ae84107985dbd36a60ad56438
ojgraham1 <ojgraham1@gmail.com>
2024-06-20 13:20:23 -0400

    logout fixed

--- src/client/pages/account.jsx
+++ src/client/pages/account.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../redux/api/auth";
 
-function Account() {
+function Account({ setToken }) {
   const [logout, { isLoading, isError }] = useLogoutMutation();
   const [logoutSuccess, setLogoutSuccess] = useState(false);
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
       await logout();
+      window.sessionStorage.removeItem("token");
+      setToken(null);
       setLogoutSuccess(true);
+      navigate("/logout")
     } catch (error) {
       console.error("Logout error:", error);
     }
   };
-
-  if (logoutSuccess) {
-    return <Link to="/logout" />;
-  }
 
   return (
     <div>
@@ -31,5 +31,3 @@ function Account() {
 }
 
 export default Account;
-
-
