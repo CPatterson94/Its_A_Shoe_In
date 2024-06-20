a671c1a392c1a695febae7bdf1c0897a561cba1b
ojgraham1 <ojgraham1@gmail.com>
2024-06-20 13:40:21 -0400

    footer fixed

--- src/client/components/navigation/footer.jsx
+++ src/client/components/navigation/footer.jsx
@@ -1,9 +1,16 @@
-function Footer(){
-    return(
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+function Footer() {
+    return (
         <footer>
-            <h3>Hello! I'm a Footer</h3>
+            <a href="#top">Back to Top</a>
+            <a href="https://github.com/CPatterson94/Its_A_Shoe_In" target="_blank" rel="noopener noreferrer" className="git">
+                <FontAwesomeIcon icon={faGithub} size="3x" style={{ color: "#000000" }} />
+            </a>
         </footer>
-    )
+    );
 }
 
 export default Footer;
