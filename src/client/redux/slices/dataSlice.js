c18a2cf198ab7119a3b0a5a267b211c48bfad6ba
10zout <tenzinthunsur@gmail.com>
2024-06-20 13:49:48 -0400

    added and removed lines of code to cart/index/dataSlice
    made changes to dataslice cart and index in hopes it allows us to utilize cart.jsx better

--- src/client/redux/slices/dataSlice.js
+++ src/client/redux/slices/dataSlice.js
@@ -34,7 +34,18 @@ const dataSlice = createSlice({
     status: "idle",
     error: null,
   },
-  reducers: {},
+  reducers: { 
+    addToCart: (state, action) => {
+    const item = state.cart.find((item) => item.id === action.payload.id);
+    if (item) {
+      item.quantity += 1;
+    } else {
+      state.cart.push({ ...action.payload, quantity: 1 });
+    }
+  },
+  removeFromCart: (state, action) => {
+    state.cart = state.cart.filter((item) => item.id !== action.payload);
+  },},
   extraReducers: (builder) => {
     builder
       .addCase(fetchCart.pending, (state) => {
