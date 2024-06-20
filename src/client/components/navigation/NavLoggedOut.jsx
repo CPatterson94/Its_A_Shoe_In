288d51daffd988f91f70e071678bb4cb31d5b7ab
ojgraham1 <ojgraham1@gmail.com>
2024-06-20 14:16:01 -0400

    logos inserted

--- src/client/components/navigation/NavLoggedOut.jsx
+++ src/client/components/navigation/NavLoggedOut.jsx
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
-
-//import {useState} from "react";
-//import {useSelector} from "react-redux";
-//import {useLogoutMutation} from
-//import SearchBar from
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStore } from '@fortawesome/free-solid-svg-icons'; 
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'; 
+import { faUser } from '@fortawesome/free-regular-svg-icons'; 
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'; 
 
 function NavLoggedOut() {
-  // const [logout] = useLogoutMutation();
-  // const user = useSelector((state)=>state.auth.credentials.user) || "";
+
 
   return (
     <>
       <nav>
-        <div>{/* <SearchBar/> */}</div>
         <div>
-          <Link to="/">Products</Link>
-          <Link to="/cart">Cart</Link>
-          <Link to={"/register"}>Register</Link>
-          <Link to={"/login"}>Login</Link>
+        <Link to="/"className="NavLink">
+        Home
+          <FontAwesomeIcon icon={faStore} size="3x" style={{ color: "#000000" }} />
+        </Link>
+          <Link to="/cart"className="NavLink">
+          Cart
+             <FontAwesomeIcon icon={faCartShopping} size="3x" style={{ color: "#000000" }} />
+          </Link>
+          <Link to={"/register"}className="NavLink">
+          Register
+          <FontAwesomeIcon icon={faUserPlus} size="3x" style={{ color: "#000000" }} />
+          </Link>
+          <Link to="/login" className="NavLink">
+          Login
+             <FontAwesomeIcon icon={faUser} size="3x" style={{ color: "#000000" }} />
+          </Link>
         </div>
       </nav>
     </>
