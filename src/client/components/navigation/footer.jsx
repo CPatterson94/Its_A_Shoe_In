fd8431686a0cf18e99baafb756f67f4956cd8c64
Curtis Patterson <Dpatter94@yahoo.com>
2024-06-20 19:46:55 -0400

    Changed some styling stuff, made a complete backgound color so no weird white raidus around entire page. Also added a new main.scss Deleted unusned file nav.jsx

--- src/client/components/navigation/footer.jsx
+++ src/client/components/navigation/footer.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    return (
-        <footer className = "footer">
-            <a href="#top">Back to Top</a>
-            <span className="git">
-            <a href="https://github.com/CPatterson94/Its_A_Shoe_In" target="_blank" rel="noopener noreferrer" className="git">
-                <FontAwesomeIcon icon={faGithub} size="3x" style={{ color: "#000000" }} />
-            </a>
-            </span>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <a href="#top">Back to Top</a>
+      <span className="git">
+        <a
+          href="https://github.com/CPatterson94/Its_A_Shoe_In"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="git"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="3x"
+            style={{ color: "#000000" }}
+          />
+        </a>
+      </span>
+    </footer>
+  );
 }
 
 export default Footer;
